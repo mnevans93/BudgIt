3abfbaed1af377095e944cafa2c3d7decced22d3
Matthias Nevans <mnevans93@gmail.com>
2023-01-10 09:56:53 -0500

    Tweaked server.js redirection behavior

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use('/api/users', require('./routes/api/users'))
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
-  res.redirect('/login')
+  res.redirect('/')
 })
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`)
